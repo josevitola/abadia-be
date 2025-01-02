7b913d8baffda9a084a070a9fb50da5637136314
José Vitola <jdnv.123@gmail.com>
2025-01-02 13:33:51 -0500

    Add textsByAuthor query

--- app/src/gql/models/texts.rs
+++ app/src/gql/models/texts.rs
@@ -1,11 +1,10 @@
 use crate::gql::AppContext;
 use async_graphql::{Context, Object, SimpleObject};
 use sqlx::{postgres::PgRow, Row};
-use uuid::Uuid;
 
 #[derive(sqlx::FromRow, Hash, Clone, SimpleObject)]
-pub struct Book {
-    pub id: Uuid,
+pub struct Text {
+    pub id: String,
     pub title: String,
 }
 
@@ -14,17 +13,37 @@ pub(crate) struct TextQuery;
 
 #[Object]
 impl TextQuery {
-    async fn texts(&self, ctx: &Context<'_>) -> Result<Vec<Book>, async_graphql::Error> {
+    async fn texts(&self, ctx: &Context<'_>) -> Result<Vec<Text>, async_graphql::Error> {
         let pool = &ctx.data::<AppContext>()?.pool;
 
-        let query: Vec<Book> = sqlx::query("SELECT * FROM texts ORDER BY id")
-            .map(|row: PgRow| Book {
+        let texts: Vec<Text> = sqlx::query("SELECT * FROM texts ORDER BY id")
+            .map(|row: PgRow| Text {
                 id: row.get("id"),
                 title: row.get("title"),
             })
             .fetch_all(pool)
             .await?;
 
-        Ok(query)
+        Ok(texts)
+    }
+
+    async fn texts_by_author(
+        &self,
+        ctx: &Context<'_>,
+        author_id: String,
+    ) -> Result<Vec<Text>, async_graphql::Error> {
+        let pool = &ctx.data::<AppContext>()?.pool;
+
+        let texts= 
+            sqlx::query("SELECT t.* FROM texts_authors ta JOIN texts t ON (ta.text_id = t.id) WHERE ta.author_id = $1")
+                .bind(author_id)
+                .map(|row: PgRow| Text {
+                    id: row.get("id"),
+                    title: row.get("title"),
+                })
+                .fetch_all(pool)
+                .await?;
+
+        Ok(texts)
     }
 }
