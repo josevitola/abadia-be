b52ae4d4148ef9ebd21e610beb81c4933f0dfba3
José Vitola <jdnv.123@gmail.com>
2025-01-05 21:01:11 -0500

    Remove unnecessary code

--- app/src/gql/models/bioentries/mutations.rs
+++ app/src/gql/models/bioentries/mutations.rs
@@ -2,8 +2,8 @@ use super::{
     db::{BiographyEntryDB, BiographyEntryForm},
     AppContext,
 };
-use crate::utils::db::{DBError, DBManager};
-use async_graphql::{Context, Error, Object};
+use crate::utils::db::DBManager;
+use async_graphql::{Context, Object};
 
 #[derive(Default)]
 pub struct BiographyEntryMutation; // glorious evolution!
@@ -18,16 +18,9 @@ impl BiographyEntryMutation {
         let pool = &ctx.data::<AppContext>()?.pool;
 
         let mut tx = pool.begin().await?;
-        let conn = &mut *tx;
+        let new_id_res = BiographyEntryDB::insert_one(&mut *tx, input).await?;
+        tx.commit().await?;
 
-        let new_id_res = BiographyEntryDB::insert_one(conn, input).await;
-
-        new_id_res.map_err(|e| match e {
-            DBError::Insert(insert_msg) => Error {
-                message: insert_msg,
-                extensions: None,
-                source: None,
-            },
-        })
+        Ok(new_id_res)
     }
 }
